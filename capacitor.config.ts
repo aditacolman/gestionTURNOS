305ba014a153f964e57532fe941eb16bd772d22a
aditacolman <acolman@sagradoalcal.edu.ar>
2024-05-24 17:12:26 -0300

    Creando pantalla agenda

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gestionTURNOS',
+  webDir: 'www'
+};
+
+export default config;
