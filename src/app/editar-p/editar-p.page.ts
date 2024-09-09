8f6b1b09ae9f7807a1bb6d9b1ded67e3ea542cd6
andres almiron <andres.almiron@sagradoalcal.edu.ar>
2024-09-09 14:25:53 -0300

    editarp

--- src/app/editar-p/editar-p.page.ts
+++ src/app/editar-p/editar-p.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PopoverController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-editar-p',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editar-p.page.scss'],
 })
 export class EditarPPage implements OnInit {
-
-  constructor() { }
+  
+  constructor(
+    public popoverController: PopoverController,
+    private navCtrl: NavController // Agregamos NavController
+  ) {}
 
   ngOnInit() {
   }
 
+  volverAtras() {
+    this.navCtrl.back();
+  }
 }
