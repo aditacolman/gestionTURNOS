6fdd10a8a59f97240c941df056f6b4961e1df6e1
aditacolman <acolman@sagradoalcal.edu.ar>
2024-10-21 12:07:08 -0300

    Continuando pantallas

--- src/app/listado-clientes/listado-clientes.page.ts
+++ src/app/listado-clientes/listado-clientes.page.ts
@@ -2,16 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { PopoverController, NavController } from '@ionic/angular';
 import { ApiService } from '../api.service';
 
+
+interface Cliente {
+  Nombre: string;
+  Apellido: string;
+  Telefono: string;
+}
+
 @Component({
   selector: 'app-listado-clientes',
   templateUrl: './listado-clientes.page.html',
   styleUrls: ['./listado-clientes.page.scss'],
 })
 
-
 export class ListadoClientesPage implements OnInit {
 
-  clientes =[];
+  Clientes: Cliente[] = [];
+  searchTerm: string = '';
+  filteredClientes: Cliente[] = [];
+  selectedFilter: string = 'nombre'; 
+
 
   constructor(private  servicio: ApiService, 
     private navCtrl: NavController ) { }
@@ -19,15 +29,29 @@ export class ListadoClientesPage implements OnInit {
   traerDatos(){
     this.servicio.traer_clientes().subscribe(respuesta=>{
       console.log(respuesta)
-      this.clientes = respuesta
+      this.Clientes = respuesta;
+      this.filteredClientes= respuesta; // Inicializa la lista filtrada
     })
-
-  
   }
 
   ngOnInit() {
     this.traerDatos()
-  }
+    }
+  
+    // Método para filtrar los trabajadores según el término de búsqueda
+  onSearch() {
+    const searchTermLower = this.searchTerm.toLowerCase();
+    this.filteredClientes = this.Clientes.filter(cliente => {
+      // Siempre busca en Nombre y Apellido
+      return cliente.Nombre.toLowerCase().includes(searchTermLower) ||
+      cliente.Apellido.toLowerCase().includes(searchTermLower);
+      });
+    }
+  
+    // Método para actualizar el filtro seleccionado (no lo usamos en la búsqueda actual)
+  onFilterChange(event: any) {
+    this.selectedFilter = event.detail.value;
+    }
 
   volverAtras() {
     this.navCtrl.back();
