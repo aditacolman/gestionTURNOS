f4b8f43c449b11a97ca44b7d3ded11e26e4defd8
lugarte1 <lugarte@sagradoalcal.edu.ar>
2024-12-02 17:22:14 -0300

    adaptacion a modo oscuro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    // Forzar el tema claro globalmente (no es necesario añadir una clase 'light' si ya lo definiste en el global.scss)
+    document.body.classList.remove('dark');  // Asegúrate de eliminar cualquier clase 'dark' si es que existe
+    document.body.classList.add('light');   // Forzar el tema claro
+
+    // Configurar la barra de estado en modo claro (puedes cambiar a Style.Dark si prefieres estilo oscuro)
+    StatusBar.setStyle({ style: Style.Light });  // Si quieres la barra de estado en estilo claro
+  }
 }
